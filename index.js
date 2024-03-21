09268329a7fe127ada95cec61aeb45f4c0b05300
Gustavo Fraga <gustavohfraga1@gmail.com>
2024-03-21 18:16:11 -0300

    receba

--- index.js
+++
@@ -1,5 +0,0 @@
-
-function mudaFoto(foto) {
-    document.getElementById("icone").src = foto;
-    document.getElementById("icone").classList.add("foto-trocada"); // Adiciona a classe 'foto-trocada'
-}
